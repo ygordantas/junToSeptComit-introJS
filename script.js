b5428e86abfbc7def8d7c6d12412a8087387d310
ygordantas <ygord.furtado@gmail.com>
2025-06-24 21:31:07 -0600

    started todo app

--- script.js
+++ script.js
@@ -1,9 +1,53 @@
-import * as myFile from "./recursion.js";
+const dialog = document.getElementById("dialog");
+const showTodoFormBtn = document.getElementById("showTodoFormBtn");
+const todoList = document.getElementById("todoList");
+const todoForm = document.getElementById("todoForm");
+const closeDialogBtn = document.getElementById("closeDialogBtn");
 
-console.log(myFile.countDown);
-console.log(myFile.fibonacci);
-console.log(myFile.default);
+let todos = [];
+let nextId = 1;
 
-// DOM manipulation with plain JS
-// React => lib make our interactions with html elements simpler
-//
+showTodoFormBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
+
+closeDialogBtn.addEventListener("click", () => {
+  dialog.close();
+});
+
+todoForm.addEventListener("submit", () => {
+  const textarea = document.getElementById("todo");
+  const newTodoContainer = document.createElement("li");
+  const todoText = document.createElement("p");
+  const timestamp = document.createElement("p");
+  const deleteBtn = document.createElement("button");
+
+  deleteBtn.addEventListener("click", () => {
+    todos = todos.filter((e) => e.id !== nextId);
+    console.log(todos);
+    console.log(nextId);
+  });
+
+  deleteBtn.innerHTML = "delete";
+
+  const currentDate = new Date();
+
+  todoText.innerHTML = textarea.value;
+  timestamp.innerHTML = currentDate.toLocaleDateString();
+
+  newTodoContainer.append(todoText, timestamp, deleteBtn);
+
+  todoList.prepend(newTodoContainer);
+
+  textarea.value = "";
+
+  todos.push({
+    id: nextId,
+    text: todoText.innerHTML,
+    timestamp: currentDate,
+  });
+
+  nextId++;
+
+  console.log(todos);
+});
