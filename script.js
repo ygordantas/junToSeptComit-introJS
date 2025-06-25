c59107359650c30cb1bfd2f7e4e2e93404fd3af3
ygordantas <ygord.furtado@gmail.com>
2025-06-24 18:22:51 -0600

    Closures

--- script.js
+++ script.js
@@ -1,96 +1,37 @@
-// // regular flow
-// // function deffer execution
+// Closures
+// lexical scoping: When function get executed they have access to
+// variables that were defined inside the parent function (scope)
 
-// // control structure
+// code encapsulation
+function createCounter() {
+  let counter = 0;
 
-// //conditional
+  function increment() {
+    counter++;
+  }
 
-// let isRaining = 0; // truthy {}, true, [], Date| falsy (0, NaN, undefined, null, "", false) => false
-// let isSnowing = 2;
+  function decrement() {
+    counter--;
+  }
 
-// if (isRaining) {
-//   console.log("bring an umbrella");
-// } else if (isSnowing) {
-//   console.log("Bring a heavy jacket");
-// } else {
-//   console.log("Enjoy the sunshine");
-// }
+  function getCurrentCounter() {
+    return counter;
+  }
 
-// const daysOfTheWeek = [
-//   "sunday",
-//   "monday",
-//   "tuesday",
-//   "wednesday",
-//   "thursday",
-//   "friday",
-//   "saturday",
-// ];
-
-// const currentDay = daysOfTheWeek[1];
-
-// switch (currentDay) {
-//   case daysOfTheWeek[0]: {
-//     console.log("going to the park");
-//     break;
-//   }
-//   case daysOfTheWeek[2]: {
-//     console.log("COMIT Class");
-//     break;
-//   }
-//   default: {
-//     console.log("TBD");
-//   }
-// }
-
-// // if (isRaining) {
-// //   console.log("bring an umbrella");
-// // }
-
-// // if (isSnowing) {
-// //   console.log("Bring a heavy jacket");
-// // } else {
-// //   console.log("Enjoy the sunshine");
-// // }
-
-// // loops
-
-// const myHobbies = ["football", "swimming", "eating pizza"]; // length 3
-
-// // myHobbies.forEach((i) => console.log(i));
-
-// // for (let counter = 0; counter < 10; counter++) {
-// //   if (counter === 2) {
-// //     break;
-// //   }
-// //   console.log(counter);
-// // }
-
-// // infinity loop
-
-// let counter = 6;
-
-// while (counter < 5) {
-//   console.log(counter);
-//   counter++;
-// }
-
-// // NaN // FALSY
-// // piece of code to be used at Least once
-// // let userAge;
-// // do {
-// //   userAge = Number(prompt("What's your age?")) || 0;
-// // } while (userAge < 18);
-
-// for (const hobbie of myHobbies) {
-//   console.log(hobbie);
-// }
-
-const person = {
-  name: "ygor",
-  age: 32,
-};
-
-for (const property in person) {
-  console.log("Key: " + property);
-  console.log("Value: " + person[property]);
+  return {
+    increment,
+    decrement,
+    getCurrentCounter,
+  };
 }
+
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+
+const counter2 = createCounter();
+counter2.increment();
+
+console.log(counter.getCurrentCounter()); //2
+console.log(counter2.getCurrentCounter()); //1
